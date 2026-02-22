a0dd076a41e1cbcf0656ebdb09c6f723f505ff44
Atharvkachare <atharvkachare9@gmail.com>
2026-02-22 08:58:24 +0530

    Learned how to make react project and components in react and created nav bar

---
+++ 01_React/01_react/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='parents'> 
+          <h1>This is parent component</h1>
+          <h2>Learn React Fast</h2>
+    </div>
+
+    <div>
+      <h1>This is child component</h1>
+      <h2>Learn Django Fast</h2>
+    </div>
+    </>
+
+
+   
+    
+  )
+}
+
+export default App
